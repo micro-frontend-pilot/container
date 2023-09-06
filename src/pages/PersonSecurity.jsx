b14f6a597ac84e106274bc7fe7bf2edaf7693a9e
lucky-sugar-park <135287235+lucky-sugar-park@users.noreply.github.com>
2023-09-06 20:02:12 +0900

    Create PersonSecurity.jsx

---
+++ src/pages/PersonSecurity.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PersonSecurity = () => {
+
+    return (
+        <div>
+            개인정보 처리방침 내용을 보여준다 !!!
+        </div>
+    )
+}
+
+export default PersonSecurity;
