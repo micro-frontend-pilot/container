5270437c732a8b33adc1b6eba76c58c8f85f7968
lucky-sugar-park <135287235+lucky-sugar-park@users.noreply.github.com>
2023-09-06 19:52:35 +0900

    Create Body.jsx

---
+++ src/Body.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import BodyRouter from './routes/BodyRouter';
+import BreadcrumbNav from 'common/BreadcrumbNav';
+
+const Body = (props) => {
+    
+    const [drawerOpen, setDrawerOpen] = React.useState(props.drawerOpen);
+
+    React.useEffect(()=>{
+        setDrawerOpen(props.drawerOpen);
+    }, [props.drawerOpen]);
+
+    const marginLeft = drawerOpen ? 240 : 56;
+
+    return (
+        <div style={{ marginLeft: marginLeft }}>
+            <BreadcrumbNav />
+            <BodyRouter { ...props } />
+        </div>
+    );
+}
+
+export default Body;
