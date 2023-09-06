630746f6d9530239e789234fb68b2addef729661
lucky-sugar-park <135287235+lucky-sugar-park@users.noreply.github.com>
2023-09-06 20:00:22 +0900

    Create BoardAddition.jsx

---
+++ src/pages/BoardAddition.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BoardAddition = (props) => {
+    return (
+        <div>
+            Board Addition Form !!!
+        </div>
+    )
+}
+
+export default BoardAddition;
