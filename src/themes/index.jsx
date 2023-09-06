2fd1cbbdf23704967a6556f6eefd2c1438268f4d
lucky-sugar-park <135287235+lucky-sugar-park@users.noreply.github.com>
2023-09-06 20:06:43 +0900

    Create index.jsx

---
+++ src/themes/index.jsx
@@ -0,0 +1,20 @@
+import DefaultTheme from './DefaultTheme';
+import Theme2 from './Theme2';
+import Theme3 from './Theme3';
+
+const themeList = [
+    {
+      themeName: "Default Theme",
+      theme: DefaultTheme
+    },
+    {
+        themeName: "Theme2",
+        theme: Theme2
+    },
+    {
+      themeName: "Theme3",
+      theme: Theme3
+  }
+]
+
+export default themeList;
