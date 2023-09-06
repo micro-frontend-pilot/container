f50a8108903426dee5eba79871122abf438472c4
lucky-sugar-park <135287235+lucky-sugar-park@users.noreply.github.com>
2023-09-06 19:54:27 +0900

    Create PutTogether.jsx

---
+++ src/PutTogether.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import UIComposer from './UIComposer';
+import themeList from './themes';
+
+class PutTogether extends React.Component {
+
+    constructor(props) {
+      super(props);
+      
+      this.state = {
+          themes: themeList,
+          curThemeInfo: themeList[0]
+      }
+  
+      this.changeTheme = this.changeTheme.bind(this);
+    }
+  
+    changeTheme(themeName) {
+      for(var i = 0; i < this.state.themes.length; i++) {
+        const theme = this.state.themes[i];
+        if(theme.themeName === themeName) {
+          this.setState({
+            curThemeInfo: theme
+          });
+          break;
+        }
+      }
+    }
+  
+    render() {
+      return(
+        <UIComposer 
+          themes={ this.state.themes } 
+          curThemeInfo={ this.state.curThemeInfo } 
+          changeTheme={ this.changeTheme }
+        />
+      );
+    }
+  }
+  
+  export default PutTogether;
